337fac0e2fd28a4e2ba8101b8b0ddd199fe49d1f
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-07-20 23:31:54 +0000

    Change the Bucket param to point to the bucket name instead of bucket arn

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -17,7 +17,7 @@ export function getGetSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
 
   return s3.getSignedUrl('getObject', {
-    Bucket: config.aws_media_bucket,
+    Bucket: config.aws_bucket_name,
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
@@ -26,9 +26,9 @@ export function getGetSignedUrl( key: string ): string {
 // Generates an AWS signed URL for uploading objects
 export function getPutSignedUrl( key: string ): string {
   const signedUrlExpireSeconds = 60 * 5;
-
   return s3.getSignedUrl('putObject', {
-    Bucket: config.aws_media_bucket,
+    // Bucket: config.aws_media_bucket,
+    Bucket: config.aws_bucket_name,
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -6,6 +6,7 @@ export const config = {
   'dialect': 'postgres',
   'aws_region': process.env.AWS_REGION,
   'aws_profile': process.env.AWS_PROFILE,
+  'aws_bucket_name': process.env.AWS_BUCKET_NAME,
   'aws_media_bucket': process.env.AWS_BUCKET,
   'url': process.env.URL,
   'jwt': {
