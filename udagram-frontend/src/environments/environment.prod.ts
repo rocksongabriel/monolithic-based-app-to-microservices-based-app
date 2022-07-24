6f6d6fb860d88b94cea76653a9ead767db9a3c13
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-07-24 21:21:56 +0000

    Modify the apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab83a77d1b48b4582987fbb977444bf4-1829142742.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
